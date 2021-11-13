c4c3b5fbdb3808ee86eb9bca31aed56a34547c16
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-11-13 19:22:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0;
+
+    cin >> n;
+
+    if (n >= 2) {
+        int f1 = 1;
+        int f2 = 1;
+        int f = 0;
+        for (int i = 1; i <= n; i++) {
+            f1 = f2;
+            f2 = f;
+            f = f1+f2;
+            cout << f << endl;
+        }
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
